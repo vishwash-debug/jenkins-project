ab71535129d8151f5bf82eb03064a6eb07596ea6
vishwash-debug <prazwalvishwash@gmail.com>
2026-03-19 16:44:24 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,16 @@
-def run_app():
-    print("Hello from a simple Python app running in Docker!")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello from Docker + Jenkins + Ansible!")
+
+def run():
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    print("Server running on port 8080...")
+    server.serve_forever()
 
 if __name__ == "__main__":
-    run_app()
+    run()
